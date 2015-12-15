219967846e992c9b433dbac8d06b8c9667125021
Ted Behling <ted@tedb.us>
2015-12-15 04:46:25 +0000

    done with day 3

--- advent/advent3.go
+++ advent/advent3.go
@@ -8,48 +8,76 @@ import (
 
 // Advent3_Houses takes a list of ^>v< and returns the number of positions visited
 func Advent3_Houses(s string) (sum1, sum2 int) {
-	r := NewRoute().Nav(s)
-	return r.How_many_unique(), 0
+	return NewRoute().Nav(s).How_many_unique(), NewRoute().DualNav(s).How_many_unique()
 }
 
 type RoutePos struct {
 	x, y int
 }
 type Route struct {
-	visited map[RoutePos]int
-	pos_x, pos_y int
+	visited map[RoutePos]struct{}
+	pos_x1, pos_y1, pos_x2, pos_y2 int
 }
 
 // NewRoute sets up our x/y movement plane and returns a Route
 func NewRoute() (r *Route) {
 	r = &Route{}
-	r.visited = make(map[RoutePos]int)
-	// don't move any, just to get the side effect of recording the origin visit
-	r.Move(0, 0)
+	r.visited = make(map[RoutePos]struct{})
+	// don't move any, just get the side effect of recording the origin visit
+	r.MoveSanta(0, 0)
 	return r
 }
 
+// Move Santa around according to ^>v< instructions 
 func (r *Route) Nav(s string) (*Route) {
 	for _, m := range s {
 		switch m {
 			case '^':
-			r.Move(0, 1)
+			r.MoveSanta(0, 1)
 			case '>':
-			r.Move(1, 0)
+			r.MoveSanta(1, 0)
 			case 'v':
-			r.Move(0, -1)
+			r.MoveSanta(0, -1)
 			case '<':
-			r.Move(-1, 0)
+			r.MoveSanta(-1, 0)
+		}
+	}
+	return r
+}
+
+// Move Santa and Robo-Santa around according to alternating ^>v< instructions
+func (r *Route) DualNav(s string) (*Route) {
+	for i, m := range s {
+		f := r.MoveSanta
+		if i % 2 != 0 {
+			f = r.MoveRoboSanta
+		}
+		switch m {
+			case '^':
+			f(0, 1)
+			case '>':
+			f(1, 0)
+			case 'v':
+			f(0, -1)
+			case '<':
+			f(-1, 0)
 		}
 	}
 	return r
 }
 
 // move changes position in a delta of x/y direction and records the visit
-func (r *Route) Move(d_x, d_y int) {
-	r.pos_x += d_x
-	r.pos_y += d_y
-	r.visited[RoutePos{r.pos_x, r.pos_y}]++
+func (r *Route) MoveSanta(d_x, d_y int) {
+	r.pos_x1 += d_x
+	r.pos_y1 += d_y
+	r.visited[RoutePos{r.pos_x1, r.pos_y1}] = struct{}{}
+}
+
+// move changes position in a delta of x/y direction and records the visit
+func (r *Route) MoveRoboSanta(d_x, d_y int) {
+	r.pos_x2 += d_x
+	r.pos_y2 += d_y
+	r.visited[RoutePos{r.pos_x2, r.pos_y2}] = struct{}{}
 }
 
 func (r *Route) How_many_unique() int {
--- advent/advent3_test.go
+++ advent/advent3_test.go
@@ -18,9 +18,27 @@ func TestAdvent3Houses(t *testing.T) {
 	for i, tt := range tests {
 		r := NewRoute().Nav(tt.in).How_many_unique()
 		if r != tt.out {
-			t.Errorf("Test %d: NewRoute(%q).Houses() => %d, want %d", i, tt.in, r, tt.out)
+			t.Errorf("Test %d: NewRoute().Nav(%q).How_many_unique() => %d, want %d", i, tt.in, r, tt.out)
 
 		}
 	}
 }
 
+func TestAdvent3HousesRoboSanta(t *testing.T) {
+	tests := []struct {
+		in  string
+		out int
+	}{
+		{"^v", 3},
+		{"^>v<", 3},
+		{"^v^v^v^v^v", 11},
+	}
+
+	for i, tt := range tests {
+		r := NewRoute().DualNav(tt.in).How_many_unique()
+		if r != tt.out {
+			t.Errorf("Test %d: NewRoute().DualNav(%q).How_many_unique() => %d, want %d", i, tt.in, r, tt.out)
+
+		}
+	}
+}
