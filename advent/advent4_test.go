00208853dcd14a4a9f1440eb90d64ec834ada7f1
Ted Behling <ted@tedb.us>
2015-12-15 14:11:15 +0000

    fixed up day4 (mining) test to add 4b test criteria (6 zeroes), and skip test due to several second runtime

--- advent/advent4_test.go
+++ advent/advent4_test.go
@@ -5,19 +5,20 @@ import (
 )
 
 func TestAdvent4Mining(t *testing.T) {
+	t.Skip("This test sometimes takes several seconds to run")
+
 	tests := []struct {
 		in         string
 		out1, out2 int
 	}{
-		{"abcdef", 609043, 0},
-		{"pqrstuv", 1048970, 0},
+		{"abcdef", 609043, 6742839},
+		{"pqrstuv", 1048970, 5714438},
 	}
 
 	for i, tt := range tests {
 		r1, r2 := Advent4_Mining(tt.in)
 		if r1 != tt.out1 || r2 != tt.out2 {
 			t.Errorf("Test %d: Advent4_Mining(%q) => %d, %d, want %d, %d", i, tt.in, r1, r2, tt.out1, tt.out2)
-
 		}
 	}
 }
