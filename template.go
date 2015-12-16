8aaeec0387ee8190efe39f213787eb06f3359896
Ted Behling <ted@tedb.us>
2015-12-16 02:35:29 +0000

    created templates to use as basis for a days solution

---
+++ template.go
@@ -0,0 +1,24 @@
+// Package advent implements attempts at the exercises found at
+// http://adventofcode.com/.  Unit tests are in advent_test.go.
+// A CLI invocation is at cmd/advent.
+package advent
+
+import (
+)
+
+// AdventX_Foo does...
+func AdventX_Foo(s string) (a, b int) {
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	for scanner.Scan() {
+		count += Foo(scanner.Text())
+	}
+
+	err := scanner.Err()
+	check_err(err)
+
+	return
+}
+
+func Foo(s string) int {
+	return 0
+}
