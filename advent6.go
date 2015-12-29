a158975d84eb1b16e70082133948e983c4c0221f
Ted Behling <ted@tedb.us>
2015-12-29 17:33:16 +0000

    completed day 6 part 1, using a bitfield/bitboard and math/big to get a 1-million bit integer, with binary math (<<, &, |)

--- advent6.go
+++ advent6.go
@@ -1,25 +1,200 @@
 package advent
 
 import (
-	"bufio"
+	"math/big"
+	"strconv"
 	"strings"
+	//	"fmt"
+	"bytes"
+	"regexp"
 )
 
-// Advent06Lights (NOT DONE) scans lines of input and follows instructions
+// Advent06Lights scans lines of input and follows instructions
 // to toggle lights in a matrix (toggle, on, off) according to specified
-// rectangles
+// rectangles, e.g.:
+//     turn on 887,9 through 959,629
+//     turn off 539,243 through 559,965
+//     toggle 393,804 through 510,976
+// This solution makes creative use of a "bitboard" that's 1 million bits
+// in length.  However, the puzzle assumes [][]bool or equivalent,
+// which burned me on part 2, which wants each light to be a variable
+// brightness instead of on/off.
 func Advent06Lights(s string) (count, b int) {
-	scanner := bufio.NewScanner(strings.NewReader(s))
-	for scanner.Scan() {
-		count += LightsExec(scanner.Text())
+	grid := NewLightGridInt(1000)
+	grid.ApplyCommands(s)
+	//println(grid.ToBinarySquare())
+	return grid.PopCount(), 0
+
+}
+
+// NewLightGridInt creates a new LightGridInt, which uses math/big
+// to implement a bit array that we operate on.  Size is one side of
+// a square grid, with total number of lights = Size * Size
+func NewLightGridInt(size int) (g *LightGridInt) {
+	return &LightGridInt{big.NewInt(0), size}
+}
+
+// LightGridInt implements a "bitboard" where the NxN matrix
+// is represented as a BigInt
+type LightGridInt struct {
+	I *big.Int
+	// grid is this length on one side; Size*Size lights
+	Size int
+}
+
+// ApplyCommands applies commands from 1 or more line of input, e.g.:
+//     turn on 887,9 through 959,629
+//     turn off 539,243 through 559,965
+//     toggle 393,804 through 510,976
+// Parser is implemented with regex this time, although could also be implemented
+// with fmt.Sscanf.
+func (g *LightGridInt) ApplyCommands(s string) {
+	re := regexp.MustCompile(`(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)`)
+	for i, m := range re.FindAllStringSubmatch(s, -1) {
+		x1, err := strconv.Atoi(m[2])
+		checkErr(err)
+		y1, err := strconv.Atoi(m[3])
+		checkErr(err)
+		x2, err := strconv.Atoi(m[4])
+		checkErr(err)
+		y2, err := strconv.Atoi(m[5])
+		checkErr(err)
+
+		switch m[1] {
+		case "turn on":
+			g.RectSetOn(x1, y1, x2, y2)
+		case "turn off":
+			g.RectSetOff(x1, y1, x2, y2)
+		case "toggle":
+			g.RectToggle(x1, y1, x2, y2)
+		default:
+			panic("bad command")
+		}
+
+		println("ApplyCommands:", i, m[1])
+		//println(i, m[0], "\n", g.ToBinarySquare())
 	}
+}
 
-	err := scanner.Err()
-	checkErr(err)
+// MaskFromRectangle returns a BigInt bit mask that can be applied
+// against LightGridInt to turn on, turn off, or toggle the masked bits.
+// 0,0 is top-left.  In this example for size=4, rect(1, 2, 3, 3) is:
+//     . . . . .
+//     . . . . .
+//     . o o o .
+//     . o o o .
+//     . . . . .
+// turns into:
+//    . . . . . . . . . . . o o o . . o o o . . . . . .
+// ...which is 14784 (least significant bit is on the right).
+func (g *LightGridInt) MaskFromRectangle(x1, y1, x2, y2 int) (mask *big.Int) {
+	rW := x2 - x1 + 1
+	rH := y2 - y1 + 1
 
+	// zeroes between the end of one row of 1's and the start of the next
+	spaceBetween := g.Size - rW
+
+	// how many zeroes between the bottom-right
+	// corner of the rectangle and the end of the square;
+	// in the example above, 6
+	spaceAtEnd := (g.Size * (g.Size - 1 - y2)) + (g.Size - 1 - x2) - spaceBetween
+	//println("spaceBetween, spaceAtEnd", spaceBetween, spaceAtEnd)
+	//_ = "breakpoint"
+
+	mask = big.NewInt(0)
+	for i := 0; i < rH; i++ {
+		// could replace this with exponent, but that uses float which
+		// could be imprecise with large values
+		for digit := 0; digit < rW; digit++ {
+			mask.Lsh(mask, uint(1))
+			mask.Add(mask, big.NewInt(1))
+			//println("mask", mask.Int64(), BigIntToBinary(mask))
+		}
+		mask.Lsh(mask, uint(spaceBetween))
+		//println("mask with space", mask.Int64(), BigIntToBinary(mask))
+
+	}
+	mask.Lsh(mask, uint(spaceAtEnd))
+	//println("mask at end", mask.Int64(), BigIntToBinary(mask))
+
+	return mask
+}
+
+// RectSetOn sets a given rectangle of lights to the On position, using binary or
+func (g *LightGridInt) RectSetOn(x1, y1, x2, y2 int) {
+	mask := g.MaskFromRectangle(x1, y1, x2, y2)
+	g.I = g.I.Or(g.I, mask)
+}
+
+// RectSetOff sets a given rectangle of lights to the Off position, using binary and-not
+func (g *LightGridInt) RectSetOff(x1, y1, x2, y2 int) {
+	mask := g.MaskFromRectangle(x1, y1, x2, y2)
+	//negMask := mask.Not(mask)
+	//println("negmask", BigIntToBinary(negMask))
+	g.I = g.I.AndNot(g.I, mask)
+}
+
+// RectToggle toggles the on/off of a given rectangle of lights, using binary xor
+func (g *LightGridInt) RectToggle(x1, y1, x2, y2 int) {
+	mask := g.MaskFromRectangle(x1, y1, x2, y2)
+	g.I = g.I.Xor(g.I, mask)
+}
+
+// ToBinary returns a string of 0/1 digits, of length
+// size*size
+func (g *LightGridInt) ToBinary() (s string) {
+	totalSize := g.Size * g.Size
+	padLen := totalSize - g.I.BitLen()
+	s = strings.Repeat("0", padLen)
+	//println("padLen, bitlen", padLen, g.I.BitLen())
+	s += BigIntToBinary(g.I)
+	return s
+}
+
+// PopCount (population count aka Hamming Weight) returns the number of 1 bits in the number.
+// Guaranteed to be <= size*size.
+func (g *LightGridInt) PopCount() (s int) {
+	for _, i := range g.I.Bits() {
+		s += PopCountInt(uintptr(i))
+	}
 	return
 }
 
+// ToBinarySquare is the same as ToBinary but
+// returns the results in the form of a square.  0,0 is top-left.
+func (g *LightGridInt) ToBinarySquare() (s string) {
+	bin := g.ToBinary()
+	//_ = "breakpoint"
+	//println("len", len(bin))
+	for i := 0; i < g.Size; i++ {
+		//println("slice", i*g.Size, ":", i*g.Size+g.Size)
+		s += bin[i*g.Size:i*g.Size+g.Size] + "\n"
+	}
+	return s
+}
+
+// PopCountInt returns the number of binary 1's in the integer x
+func PopCountInt(x uintptr) (count int) {
+	for count = 0; x > 0; count++ {
+		x &= x - 1
+	}
+	return count
+}
+
+// BigIntToBinary converts a big.Int to a string of 0/1 binary digits
+func BigIntToBinary(in *big.Int) (s string) {
+	var b bytes.Buffer
+	// this is a horrible, slow way to do this, but I don't know if it matters
+	for i := in.BitLen() - 1; i >= 0; i-- {
+		b.WriteString(strconv.Itoa(int(in.Bit(i))))
+	}
+	/*for _, n := range in.Bits() {
+		s += fmt.Sprintf("%0.64b", n)
+	}*/
+	return b.String()
+}
+
+/*
 // LightGrid represents a grid of lights (1000x1000) and accepts
 // instructions for toggling them
 type LightGrid struct {
@@ -36,3 +211,4 @@ func NewLightGrid() (g *LightGrid) {
 func LightsExec(s string) int {
 	return 0
 }
+*/
--- advent6_test.go
+++ advent6_test.go
@@ -1,6 +1,7 @@
 package advent
 
 import (
+	"math/big"
 	"testing"
 )
 
@@ -9,14 +10,161 @@ func TestAdvent6Lights(t *testing.T) {
 		in         string
 		out1, out2 int
 	}{
-		{"theinput", 0, 0},
+		{"turn on 0,0 through 4,4\nturn off 1,2 through 3,3\ntoggle 0,0 through 2,2", 14, 0},
 	}
 
+	// 0001100000
+	// 0001100000
+	// 0110100000
+	// 1000100000
+	// 1111100000
+	// 0000000000
 	for i, tt := range tests {
-		r1, r2 := Foo(tt.in), 0
+		r1, r2 := Advent06Lights(tt.in)
 		if r1 != tt.out1 || r2 != tt.out2 {
-			t.Errorf("Test %d: Foo(%q) => %d, %d, want %d, %d", i, tt.in, r1, r2, tt.out1, tt.out2)
+			t.Errorf("Test %d: Advent06Lights(%q) => %d, %d, want %d, %d", i, tt.in, r1, r2, tt.out1, tt.out2)
 
 		}
+	}
+}
+
+func TestAdvent6ToBinary(t *testing.T) {
+	bigIntBin := BigIntToBinary(big.NewInt(int64(8214555720323791245)))
+	want := "111000111111111111101101110011110110001100011010111100110001101"
+	if bigIntBin != want {
+		t.Errorf("BigIntToBinary(6) => [%s], want [%s]", bigIntBin, want)
+	}
+}
+
+func TestAdvent6ToBinaryZeroes(t *testing.T) {
+	bigIntBin := BigIntToBinary(big.NewInt(int64(32)))
+	want := "100000"
+	if bigIntBin != want {
+		t.Errorf("BigIntToBinary(32) => [%s], want [%s]", bigIntBin, want)
+	}
+}
+
+func TestAdvent6PopCount(t *testing.T) {
+	count1 := PopCountInt(8214555720323791245)
+	want := 41
+	if count1 != want {
+		t.Errorf("PopCountInt() => [%d], want [%d]", count1, want)
+	}
+
+	g := LightGridInt{big.NewInt(8214555720323791245), 0}
+	count2 := g.PopCount()
+	// "111000111111111111101101110011110110001100011010111100110001101"
+	if count2 != want {
+		t.Errorf("g.PopCount() => [%d], want [%d]", count2, want)
+	}
+}
+
+func TestAdvent6LightGridInt(t *testing.T) {
+	g := NewLightGridInt(5)
+	g.I = g.I.Add(g.I, big.NewInt(225))
+
+	val := g.I.Int64()
+	wantI := int64(225)
+	if val != wantI {
+		t.Errorf("Int64() => [%d], want [%d]", val, wantI)
+	}
+
+	bin := g.ToBinary()
+	want := "0000000000000000011100001"
+	if bin != want {
+		t.Errorf("ToBinary() => [%s], want [%s]", bin, want)
+	}
+
+	binSq := g.ToBinarySquare()
+	want = "00000\n00000\n00000\n00111\n00001\n"
+	if binSq != want {
+		t.Errorf("ToBinarySquare() => [%s], want [%s]", binSq, want)
+	}
+
+	mask := g.MaskFromRectangle(1, 2, 3, 3)
+	wantd := big.NewInt(14784)
+	if wantd.Cmp(mask) != 0 {
+		t.Errorf("MaskFromRectangle() => [%d], want [%d]", mask, wantd)
+	}
+}
+
+func TestAdvent6LightGridIntHuge(t *testing.T) {
+	g := NewLightGridInt(1000)
+	g.I = g.I.Add(g.I, big.NewInt(225))
+
+	val := g.I.Int64()
+	wantI64 := int64(225)
+	if val != wantI64 {
+		t.Errorf("Int64() => [%d], want [%d]", val, wantI64)
+	}
+
+	bin := g.ToBinary()
+	wantI := 1000000
+	if len(bin) != wantI {
+		t.Errorf("ToBinary() => [%d], want [%d]", len(bin), wantI)
+	}
+
+	binSq := g.ToBinarySquare()
+	wantI = 1000000 + 1000
+	if len(binSq) != wantI {
+		t.Errorf("ToBinarySquare() => [%d], want [%d]", len(binSq), wantI)
+	}
+}
+
+func TestAdvent6SetOn(t *testing.T) {
+	g := NewLightGridInt(5)
+	g.RectSetOn(1, 2, 3, 3)
+	//println("int64", g.I.Int64())
+	binSq := g.ToBinarySquare()
+	//println("binSq:", binSq)
+	want := "00000\n00000\n01110\n01110\n00000\n"
+	if binSq != want {
+		t.Errorf("RectSetOn() => [%s], want [%s]", binSq, want)
+	}
+
+	c := g.PopCount()
+	wantC := 6
+	if c != wantC {
+		t.Errorf("PopCount() => %d, want %d", c, wantC)
+	}
+}
+
+func TestAdvent6SetOff(t *testing.T) {
+	g := NewLightGridInt(5)
+	g.RectSetOn(0, 0, 4, 4)
+	g.RectSetOff(1, 2, 3, 3)
+	//println("int64", g.I.Int64())
+	binSq := g.ToBinarySquare()
+	//println("binSq:", binSq)
+	want := "11111\n11111\n10001\n10001\n11111\n"
+	if binSq != want {
+		t.Errorf("RectSetOff() => [%s], want [%s]", binSq, want)
+	}
+
+	c := g.PopCount()
+	wantC := 19
+	if c != wantC {
+		t.Errorf("PopCount() => %d, want %d", c, wantC)
+	}
+}
+
+func TestAdvent6Toggle(t *testing.T) {
+	g := NewLightGridInt(5)
+	g.RectSetOn(0, 0, 4, 4)
+	g.RectSetOff(1, 2, 3, 3)
+	g.RectToggle(0, 0, 2, 2)
+
+	//println("int64", g.I.Int64())
+	binSq := g.ToBinarySquare()
+	//println("binSq:", binSq)
+	want := "00011\n00011\n01101\n10001\n11111\n"
+	if binSq != want {
+		t.Errorf("RectToggle() => [%s], want [%s]", binSq, want)
+	}
+
+	c := g.PopCount()
+	wantC := 14
+	if c != wantC {
+		t.Errorf("PopCount() => %d, want %d", c, wantC)
 	}
 }
