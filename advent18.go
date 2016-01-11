1024b90979142d790a14d4b4fe934ab22811d207
Ted Behling <ted@tedb.us>
2016-01-11 00:56:50 +0000

    initial work on Day 18 (Conways Game of Life)

--- advent18.go
+++ advent18.go
@@ -1,19 +1,76 @@
 package advent
 
 import (
-	"bufio"
+	"fmt"
 	"strings"
 )
 
-// Advent18Animation (NOT DONE) is a template function for other Advent of Code puzzles to start from
-func Advent18Animation(s string) (count, b int) {
-	scanner := bufio.NewScanner(strings.NewReader(s))
-	for scanner.Scan() {
-		count += Foo(scanner.Text())
+// Advent18Animation implements a version of Conway's Game of Life.
+func Advent18Animation(s string, steps int) (count, b int) {
+	c, err := NewConway(s, '#', '.')
+	if err != nil {
+		panic(err)
+	}
+	c.Step(steps)
+	return c.Lit(), 0
+}
+
+// NewConway takes a string representing a 2d array of lights,
+// where the Lit and Unlit runes represent the characters that show whether the cell is lit
+// Valid input chars are the Lit and Unlit runes, and newlines; others err
+// Edge cases where blank lines or empty matrixes are input will likely fail
+func NewConway(input string, Lit rune, Unlit rune) (ConwayBoard, error) {
+	size := strings.IndexRune(input, '\n')
+	c := NewConwayBoard(size)
+	row, col := 0, 0
+
+	for _, r := range input {
+		switch r {
+		case '\n':
+			row++
+			col = 0
+		case Lit:
+			c[row][col] = true
+		case Unlit:
+			c[row][col] = false
+		default:
+			return nil, fmt.Errorf("Char %s invalid for NewConway (not \\n, %s, or %s)", r, Lit, Unlit)
+		}
 	}
 
-	err := scanner.Err()
-	checkErr(err)
+	return c, nil
+}
 
-	return
+// ConwayBoard is a 2d array (matrix) of bool.  True is lit, False is unlit
+type ConwayBoard []ConwayRow
+type ConwayRow []bool
+
+// NewConwayBoard creates a new Conway ([][]bool) of given size (square)
+func NewConwayBoard(size int) ConwayBoard {
+	c := make(ConwayBoard, size)
+	for i := 0; i < size; i++ {
+		c[i] = make(ConwayRow, size)
+	}
+	return c
+}
+
+// Step plays through the Game of Life rules zero or more times
+func (c ConwayBoard) Step(steps int) ConwayBoard {
+	newBoard := NewConwayBoard(c.Width())
+	return newBoard
+}
+
+// Lit returns number of cells that are true
+func (c ConwayBoard) Lit() int {
+	return 0
+}
+
+// Width returns width of board (same as Height since it's a square)
+func (c ConwayBoard) Width() int {
+	return len(c[0])
+}
+
+// Height returns height of board (same as Width since it's a square)
+func (c ConwayBoard) Height() int {
+	return len(c)
 }
--- advent18_test.go
+++ advent18_test.go
@@ -5,18 +5,80 @@ import (
 )
 
 func TestAdvent18Animation(t *testing.T) {
+	steps := []string{
+		// 0
+		`.#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####..
+`,
+		// 1
+		`..##..
+..##.#
+...##.
+......
+#.....
+#.##..
+`,
+		// 2
+		`..###.
+......
+..###.
+......
+.#....
+.#....
+`,
+		// 3
+		`...#..
+......
+...#..
+..##..
+......
+......
+`,
+		// 4
+		`......
+......
+..##..
+..##..
+......
+......
+`}
 	tests := []struct {
 		in         string
 		out1, out2 int
 	}{
-		{"theinput", 0, 0},
+		// input string + 1 step, test number illuminated
+		{steps[0], 11, 0},
+		{steps[1], 8, 0},
+		{steps[2], 4, 0},
+		{steps[3], 4, 0},
 	}
 
 	for i, tt := range tests {
-		r1, r2 := Foo(tt.in), 0
+		r1, r2 := Advent18Animation(tt.in, 1)
 		if r1 != tt.out1 || r2 != tt.out2 {
-			t.Errorf("Test %d: Foo(%q) => %d, %d, want %d, %d", i, tt.in, r1, r2, tt.out1, tt.out2)
-
+			t.Errorf("Test %d: Advent18Animation(%q, 1) => %d, %d, want %d, %d", i, tt.in, r1, r2, tt.out1, tt.out2)
 		}
+	}
+
+	c, err := NewConway(steps[0], '#', '.')
+	if err != nil {
+		t.Fatalf("Test: NewConway errored: %s", err)
+	}
+
+	if c.Lit() != 15 {
+		t.Errorf("Test: NewConway(%q) => %d, want %d", steps[0], c.Lit(), 15)
+	}
+	if c.Width() != 6 || c.Height() != 6 {
+		t.Errorf("Test: NewConway(%q) => %dx%d, want %dx%d", steps[0], c.Width(), c.Height(), 6, 6)
+	}
+
+	c = c.Step(4)
+
+	if c.Lit() != 4 {
+		t.Errorf("Test: NewConway(%q) + Step(4) => %d, want %d", steps[0], c.Lit(), 4)
 	}
 }
