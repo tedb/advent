719bc974712cd9399283ec2ee7db0a738bbeccf3
Ted Behling <ted@tedb.us>
2017-12-01 22:45:32 -0500

    baseline command line dispatch and day 1 placeholder

--- advent.nim
+++ advent.nim
@@ -1,1 +1,35 @@
-echo "hello world"
+import adventpkg/day1
+
+proc dispatch*(day: string, a: bool, b: bool, input: string = nil): string =
+  var a = a
+  var b = b
+  if not a and not b:
+    a = true
+    b = true
+
+  case day
+  of "1":
+    if a:
+      result = day1.day1InverseCaptchaA(input)
+    if b:
+      result = day1.day1InverseCaptchaB(input)
+  else:
+    echo "Day", day, "is not implemented"
+
+when isMainModule:
+  import docopt, tables, strutils
+
+  let doc = """
+Advent of Code 2017
+
+Usage:
+  advent <day> [a | b] [<input>]
+
+Options:
+  -h --help     Show this screen.
+  -v --verison  Show version.
+"""
+
+  let args = docopt(doc, version = "advent 0.1.0")
+
+  echo dispatch($ args["<day>"], args["a"], args["b"], $ args["<input>"])
---
+++ adventpkg/day1.nim
@@ -0,0 +1,5 @@
+proc day1InverseCaptchaA*(input: string): string =
+  result = input
+
+proc day1InverseCaptchaB*(input: string): string =
+  result = input
