d2bb0a74e6355b5bce2845cb844771ef81b2d24b
Ted Behling <ted@tedb.us>
2017-12-02 22:13:58 -0500

    Solve Day 1 using a Nim iterator

--- adventpkg/day1.nim
+++ adventpkg/day1.nim
@@ -1,5 +1,24 @@
-proc day1InverseCaptchaA*(input: string): string =
-  result = input
+import strutils, parseutils
+
+type CircularDigitString = object
+  digits: string
+  offset: int
+
+proc rightindexforpair(digitstring: CircularDigitString, left: int): int =
+  (left + digitstring.offset) mod digitstring.digits.len
+
+iterator pairs(digitstring: CircularDigitString): tuple[a: char, b: char] =
+  var i = 0
+  while i < digitstring.digits.len:
+    yield (digitstring.digits[i], digitstring.digits[digitstring.rightindexforpair(i)])
+    inc i
+
+proc day1InverseCaptchaA*(input: string, offset: int = 1): string =
+  var sum = 0
+  for a, b in CircularDigitString(digits: input, offset: offset):
+    if a == b:
+      sum += parseInt($a)
+  result = $sum
 
 proc day1InverseCaptchaB*(input: string): string =
-  result = input
+  day1InverseCaptchaA(input, int(input.len/2))
--- tests/tester.nim
+++ tests/tester.nim
@@ -1,6 +1,6 @@
 import unittest, ../advent, ../adventpkg/day1
 
-suite "day1":
+suite "day1a":
   test "1122":
     let input = "1122"
     check day1.day1InverseCaptchaA(input) == "3"
@@ -13,3 +13,20 @@ suite "day1":
   test "91212129":
     let input = "91212129"
     check day1.day1InverseCaptchaA(input) == "9"
+
+suite "day1b":
+  test "1212":
+    let input = "1212"
+    check day1.day1InverseCaptchaB(input) == "6"
+  test "1221":
+    let input = "1221"
+    check day1.day1InverseCaptchaB(input) == "0"
+  test "123425":
+    let input = "123425"
+    check day1.day1InverseCaptchaB(input) == "4"
+  test "123123":
+    let input = "123123"
+    check day1.day1InverseCaptchaB(input) == "12"
+  test "12131415":
+    let input = "12131415"
+    check day1.day1InverseCaptchaB(input) == "4"
