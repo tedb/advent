3e32889dc8f99c27070a1c249b06838e0aa6faae
Ted Behling <ted@tedb.us>
2017-12-03 12:37:30 -0500

    make day 2 checksum take a function arg

--- adventpkg/day2.nim
+++ adventpkg/day2.nim
@@ -1,13 +1,25 @@
 import strutils, parseutils, sequtils
 
-proc day2CorruptionChecksumA*(input: string): string =
+proc findDivisibles(ints: seq[int]): int =
+  for i in ints:
+    for j in ints:
+      if i != j and i mod j == 0:
+        return i div j
+
+proc maxDiff(ints: seq[int]): int =
+  ints.max - ints.min
+
+proc checksum(input: string, linechecker: proc): string =
   var sum = 0
 
   for line in splitLines(input):
     var ints = line.splitWhitespace().map(parseInt)
-    sum += ints.max - ints.min
+    sum += linechecker(ints)
 
   $ sum
 
+proc day2CorruptionChecksumA*(input: string): string =
+  checksum(input, maxDiff)
+
 proc day2CorruptionChecksumB*(input: string): string =
-  ""
+  checksum(input, findDivisibles)
