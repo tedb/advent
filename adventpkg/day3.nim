5af84f7e7fdd89c1413fa4313e8442d17dd40283
Ted Behling <ted@tedb.us>
2017-12-03 13:17:55 -0500

    start day 3

--- adventpkg/day3.nim
+++ adventpkg/day3.nim
@@ -1,7 +1,18 @@
 import strutils, parseutils, sequtils
 
+# Level is number of rings distant from 1 at center
+type Level = int
+
+proc width(level: Level): int =
+  (level * 2) + 1
+
+proc perimeter(level: Level): int =
+  level * 8
+
 proc day3SpiralMemoryA*(input: string): string =
-  ""
+  let L = Level(parseInt(input))
+  $ L
 
 proc day3SpiralMemoryB*(input: string): string =
-  ""
+  let L = Level(parseInt(input))
+  $ L
