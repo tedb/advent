17117617e3e1e172d215561251e1571f577b1065
Ted Behling <221291+tedb@users.noreply.github.com>
2025-12-06 19:41:19 -0500

    day 6 part 2, with interest Enumerable usage

--- day6/day6.rb
+++ day6/day6.rb
@@ -1,10 +1,21 @@
 #!env ruby
 
 require 'byebug'
-data = ARGF.read.lines.map { |line| line.split }
+
+lines = ARGF.read.lines
+data = lines.map { |line| line.split }
 summed = data.transpose.sum do |col|
   op = col.pop.to_sym
   col.map(&:to_i).reduce(&op)
 end
 
 puts "Part 1", summed
+
+chunks = lines.map{|line| line.chomp.split('')}.transpose.slice_when {|before, _| before.join.strip == "" }
+sum = chunks.map do |chunk|
+  chunk.reject!{|c| c.join.strip == ""}
+  op = chunk.first.last.to_sym
+  chunk.map(&:join).map(&:to_i).reduce(&op)
+end.sum
+
+puts "Part 2", sum
